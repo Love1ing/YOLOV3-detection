0e1b127e88285e10cee29ed08516856cada3ced9
Love1ing <57225387+Love1ing@users.noreply.github.com>
2020-04-21 09:09:56 +0800

    Create train.py

---
+++ train.py
@@ -0,0 +1,14 @@
+import tensorflow as tf 
+import os
+import random
+import numpy as np
+
+batch_size = 64
+max_len_seq = 50
+epoch =100
+def train():
+    for i in range(epoch):
+        print('done')
+if __name__=='__main__':
+    train()
+    
